8b2fc21778e6ff86c82e54e9de6ecddc91da921d
水明 <oncwnuB_hPO0Irpwi5teTV7vc89E@git.weixin.qq.com>
2020-04-04 00:40:42 +0800

    地图生涯组件

---
+++ miniprogram/pages/careerMaps/components/context/context.js
@@ -0,0 +1,37 @@
+// pages/careerMaps/components/context/context.js
+
+const computedBehavior = require('miniprogram-computed')
+
+Component({
+  behaviors: [computedBehavior],
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    careerMaps:{
+      type:Object,
+      value:{careerMaps:{}}
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    careerMaps:{}
+  },
+
+  computed:{
+    themes:(data)=>{
+      // console.log('compoment')
+      // console.log(data)
+      return Object.keys(data.careerMaps)
+    }
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+  }
+})
